635a13bbbf7d9f9fb0ba0d89e57274694de70c88
Luke Dorny <luke@lukedorny.com>
2024-02-16 15:07:35 -0800

    Update feed.json.php
    altered the url in the template for the feed.

--- site/templates/feed.json.php
+++ site/templates/feed.json.php
@@ -6,6 +6,6 @@ echo json_encode([
     'version' => 'https://jsonfeed.org/version/1.1',
     'title' => $site->title()->value(),
     'home_page_url' => $site->url(),
-    'feed_url' => $site->url() . '/feed.json',
+    'feed_url' => $site->url() . '/feed',
     'items' => $items,
 ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
