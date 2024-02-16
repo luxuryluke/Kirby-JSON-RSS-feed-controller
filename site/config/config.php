9bd93664a8a6bf24172c8e34970f748c5b1c21b8
Luke Dorny <luke@lukedorny.com>
2024-02-16 10:27:14 -0800

    Create config.php

---
+++ site/config/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * The config file is optional. It accepts a return array with config options
+ * Note: Never include more than one return statement, all options go within this single return array
+ * In this example, we set debugging to true, so that errors are displayed onscreen.
+ * This setting must be set to false in production.
+ * All config options: https://getkirby.com/docs/reference/system/options
+ */
+return [
+    'debug' => true, // turn off in production
+    'panel' => [
+        'slug' => 'panel', // change your panel url
+        'menu' => [ // add a users section to the left menu
+            'site',
+            'system',
+            'lab' => [
+                'true'
+            ],
+            'users',
+        ]
+    ],
+
+    'routes' => [
+        [
+            'pattern' => 'feed.json',
+            'action'  => function() {
+                return page('feed')->render(['template' => 'feed.json']);
+            }
+        ],
+    ],
+];
