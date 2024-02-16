d82e30dbfabe717b6b68d2e0372df9e815e4f34c
Luke Dorny <luke@lukedorny.com>
2024-02-16 14:35:52 -0800

    Update feed.json.php to v1.1
    jsonfeed format is now 1.1 and is application/feed+json

--- site/templates/feed.json.php
+++ site/templates/feed.json.php
@@ -1,9 +1,9 @@
 <?php
 
-header('Content-type: application/json; charset=utf-8');
+header('Content-type: application/feed+json; charset=utf-8');
 
 echo json_encode([
-    'version' => 'https://jsonfeed.org/version/1',
+    'version' => 'https://jsonfeed.org/version/1.1',
     'title' => $site->title()->value(),
     'home_page_url' => $site->url(),
     'feed_url' => $site->url() . '/feed.json',
